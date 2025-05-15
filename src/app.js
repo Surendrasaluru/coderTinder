62fb184b4e028178a022e06740f8d8aa8fe314ba
Surendra <surendrasaluru@gmail.com>
2025-05-15 20:24:52 +0530

    post user get feed apis added

--- src/app.js
+++ src/app.js
@@ -3,17 +3,43 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "mani",
-    lastName: "saluru",
-    email: "mani@gmail.com",
-    password: "mani123",
-  };
+  const user = new User(req.body); //creating instance with user model
+  try {
+    await user.save();
+    res.send("user added succesfully");
+  } catch (err) {
+    res.status(400).send("error occured while saving user");
+  }
 
-  const user = new User(userObj); //creating instance with user model
-  await user.save();
-  res.send("user added succesfully");
+  console.log(req.body);
+});
+
+//FEED API
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.findOne({ email: userEmail }); //findonereturns only one user
+
+    if (user.length === 0) {
+      res.status(400).send("user not found with given mail");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("error in getting user");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find(); //find with emptyfilter returns all
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("error in getting feed");
+  }
 });
 
 connectDB()
